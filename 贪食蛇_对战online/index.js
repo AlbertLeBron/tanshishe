38f8a5c3f48c0643e07ed627abf6cf5bf970845d
AlbertLeBron <2284684897@qq.com>
2019-12-22 17:37:07 +0800

    add the images of game and some code

--- 贪食蛇_对战online/index.js
+++ 贪食蛇_对战online/index.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
- 
+
 app.get('/', function(req, res){
     res.send('<h1>Welcome Realtime Server</h1>');
 });
